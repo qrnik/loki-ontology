b335f333a42becb052add78075616daf70cc653a
Paweł <pwamej@gmail.com>
2017-09-09 09:24:53 +0200

    refactor

--- action.php
+++ action.php
@@ -8,6 +8,9 @@ class action_plugin_lokiontology extends DokuWiki_Action_Plugin
                 "<classRelations></classRelations><objectProperties></objectProperties>" .
                 "<dataProperties></dataProperties><propertyRelations></propertyRelations>" .
                 "</ontology>";
+    const ONTOLOGY_XSLT_PATH = "lib/plugins/lokiontology/ontology.xslt";
+    const ONTOLOGY_EDIT_XSLT_PATH = "lib/plugins/lokiontology/ontology_edit.xslt";
+    const ONTOLOGY_JS_PATH = "lib/plugins/lokiontology/ontology_edit.js";
 
     function register(Doku_Event_Handler $controller)
     {
@@ -21,64 +24,65 @@ class action_plugin_lokiontology extends DokuWiki_Action_Plugin
         if(trim($event->data) == "")
             $event->data = self::EMPTY_ONTOLOGY;
 
-        if(preg_match("/^<ontology>.*<\/ontology>$/", str_replace("\n", " ", $event->data)) != 1)
+        $flatData = str_replace("\n", " ", $event->data);
+        $isNotOntology = preg_match("/^<ontology>.*<\/ontology>$/", $flatData) != 1;
+        if($isNotOntology)
             return false;
 
-        $xslt = file_get_contents("lib/plugins/lokiontology/ontology.xslt");
-        $event->data = preg_replace("/<ontology>.*<\/ontology>/",
-            "&&XML&&\n\n{$event->data}\n\n&&XSLT&&\n\n$xslt\n\n&&END&&",
-            str_replace("\n", " ", $event->data));
+        $event->data = $this->transformWithXSLT($event->data);
     }
 
     function onEdit(Doku_Event $event, $param)
     {
-        // Nie modyfikuj formularza, jeżeli nie edytujemy ontologii
-        $specOntPos = strpos($event->data->_hidden["id"], "special:ontology:");
+        $isOntologyPage = strpos($event->data->_hidden["id"], "special:ontology:") === 0;
 
-        if($specOntPos === 0)
-            $this->swapEditor($event, $param);
+        if($isOntologyPage)
+            $this->swapToOntologyEditor($event, $param);
 
-        else if($specOntPos === false)
+        else
             $this->insertAutocomplete($event, $param);
-
-        return true;
     }
 
-    function swapEditor(Doku_Event $event, $param)
+    private function swapToOntologyEditor(Doku_Event $event, $param)
     {
-        if(trim($event->data->_content[0]["_text"]) == "")
-            $event->data->_content[0]["_text"] = self::EMPTY_ONTOLOGY;
+        define('PAGE_EDIT_FIELD', 0);
+        define('PREVIEW_BUTTON', 6);
+        $formContent =& $event->data->_content;
+        $pageEditField = $formContent[PAGE_EDIT_FIELD];
+        if(trim($pageEditField["_text"]) == "")
+            $pageEditField["_text"] = self::EMPTY_ONTOLOGY;
 
-        $xml = new DomDocument();
-        $xml->loadXML($event->data->_content[0]["_text"]);
+        unset($formContent[PAGE_EDIT_FIELD]);
+        unset($formContent[PREVIEW_BUTTON]);
 
         $xslt = new DomDocument();
-        $xslt->load("lib/plugins/lokiontology/ontology_edit.xslt");
+        $xslt->load(self::ONTOLOGY_EDIT_XSLT_PATH);
+        $XSLTProcessor = new XSLTProcessor();
+        $XSLTProcessor->importStylesheet($xslt);
 
-        $js = file_get_contents("lib/plugins/lokiontology/ontology_edit.js");
+        $xml = new DomDocument();
+        $xml->loadXML($pageEditField["_text"]);
+        $js = file_get_contents(self::ONTOLOGY_JS_PATH);
+        $html = "<script type='text/javascript'>\n$js\n</script>" . $XSLTProcessor->transformToXML($xml);
 
-        // Usunięcie pola tekstowego do edycji ontologii
-        unset($event->data->_content[0]);
-
-        // Usunięcie przycisku podglądu
-        unset($event->data->_content[6]);
-
-        // Określ pozycję
-        $pos = $event->data->findElementByAttribute('id', 'wiki__editbar');
-
-        // Kod nowego formularza
-        $proc = new XSLTProcessor();
-        $proc->importStylesheet($xslt);
-        $html = "<script type='text/javascript'>\n$js\n</script>";
-        $html .= $proc->transformToXML($xml);
-
-        // Wpisanie formularza
-        $event->data->insertElement($pos, $html);
+        $wikiEditbar = $event->data->findElementByAttribute('id', 'wiki__editbar');
+        $event->data->insertElement($wikiEditbar, $html);
     }
 
-    function insertAutocomplete(Doku_Event $event, $param)
+    private function insertAutocomplete(Doku_Event $event, $param)
     {
 
+    }
+
+    private function transformWithXSLT($input)
+    {
+        $xslt = file_get_contents(self::ONTOLOGY_XSLT_PATH);
+        $flatInput = str_replace("\n", " ", $input);
+        return preg_replace(
+            "/<ontology>.*<\/ontology>/",
+            "&&XML&&\n\n{$input}\n\n&&XSLT&&\n\n$xslt\n\n&&END&&",
+            $flatInput
+        );
     }
 }
 
