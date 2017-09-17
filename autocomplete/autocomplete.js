51aedb18d2f9127d61c2deb92f408a3e94e6ab0b
Paweł <pwamej@gmail.com>
2017-09-17 23:03:58 +0200

    extract ontology setup to class/Ontology.js

--- autocomplete/autocomplete.js
+++ autocomplete/autocomplete.js
@@ -1,6 +1,7 @@
 "use strict";
 const Textcomplete = require('textcomplete/lib/textcomplete');
 const Textarea = require('textcomplete/lib/textarea');
+const Ontology = require('./class/Ontology.js');
 
 const classSearch = function (term, callback) {
     const defaultClasses = defaultOntology ? defaultOntology.classes.map(c => c.id) : [];
@@ -21,28 +22,11 @@ function triggerTextcompleteAfterReplace() {
     }, 200);
 }
 
-const setup = function (data) {
-    window.ontology = data;
-    ontology.forEach(
-        ont => {
-            const subclassRelations = ont.classRelations.filter(r => r.type === 'rdfs:subclassOf');
-            ont.classes.forEach(c => {
-                c.superclasses = [c.id];
-                let directSuperclasses = subclassRelations.filter(r => r.subject === c.id).map(r => r.object);
-                c.superclasses.push(...directSuperclasses);
-                for (let i = 1; i < c.superclasses.length; i++) {
-                    let superclass = c.superclasses[i];
-                    let superSuperclasses = subclassRelations
-                        .filter(r => r.subject === superclass)
-                        .map(r => r.object)
-                        .filter(o => c.superclasses.indexOf(o) === -1);
-                    c.superclasses.push(...superSuperclasses);
-                }
-            })
-        }
-    );
-    window.defaultOntology = ontology.find(ont => ont.id === 'default');
-    window.qualifiedOntology = ontology.filter(ont => ont.id !== 'default');
+const setup = function (jsonArray) {
+    window.ontologies = jsonArray.map(json => new Ontology(json));
+    window.defaultOntology = ontologies.find(ont => ont.id === 'default');
+    window.qualifiedOntology = ontologies.filter(ont => ont.id !== 'default');
+    window.categories = [];
     const textarea = document.getElementById('wiki__text');
     const editor = new Textarea.default(textarea);
     const textcomplete = new Textcomplete.default(editor);
---
+++ autocomplete/class/Ontology.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = class Ontology {
+    constructor(json) {
+        for (let key in json) {
+            if (json.hasOwnProperty(key)) {
+                this[key] = json[key];
+            }
+        }
+        this._subclassRelations = this.classRelations.filter(r => r.type === 'rdfs:subclassOf');
+        this.classes.forEach(c => c.superclasses = this._getSuperclasses(c.id));
+    }
+
+    _getSuperclasses(classId) {
+        let superclasses = [classId];
+        let directSuperclasses = this._subclassRelations.filter(r => r.subject === classId).map(r => r.object);
+        superclasses.push(...directSuperclasses);
+        for (let i = 1; i < superclasses.length; i++) {
+            let superclassId = superclasses[i];
+            let superSuperclasses = this._subclassRelations
+                .filter(r => r.subject === superclassId)
+                .map(r => r.object)
+                .filter(o => superclasses.indexOf(o) === -1); //add super-superclass only if not present
+            superclasses.push(...superSuperclasses);
+        }
+        return superclasses;
+    }
+}
