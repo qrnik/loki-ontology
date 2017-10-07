85b573ff340c0f37fe28b20932282eee794ea0e1
Paweł <pwamej@gmail.com>
2017-10-07 21:05:36 +0200

    add attribute highlight

--- autocomplete/class/Scanner.js
+++ autocomplete/class/Scanner.js
@@ -4,6 +4,7 @@ require('highlight-within-textarea');
 module.exports = class Scanner {
     constructor(textarea, ontologies) {
         this._categoryRegexp = Scanner._createCategoryRegexp(Scanner.symbols.ID);
+        this._attributeRegexp = Scanner._createAttributeRegexp(Scanner.symbols.ID);
         this._queryRegexp = /{{#ask:[^}]*}}/g;
         this._textarea = textarea;
         this._ontologies = ontologies;
@@ -18,30 +19,46 @@ module.exports = class Scanner {
     _scan() {
         const text = this._textarea.value;
         const textWithoutQueries = text.replace(this._queryRegexp, "");
-        this.categories = this._findCategories(textWithoutQueries);
+        this.categories = Scanner._findAllMatches(textWithoutQueries, this._categoryRegexp)
+            .map(match => match[1]);
         this.emitter.emit('scan');
     }
 
-    _findCategories(text) {
+    static _findAllMatches(text, regex) {
         let result = [];
-        let match = this._categoryRegexp.exec(text);
+        let match = regex.exec(text);
         while(match) {
-            result.push(match[1]);
-            match = this._categoryRegexp.exec(text);
+            result.push(match);
+            match = regex.exec(text);
         }
         return result;
     }
 
     _highlight(text) {
-        const categories = this._findCategories(text);
-        const setDifference = jQuery(categories).not(this._ontologies.classes);
-        const categoriesToHighlight = jQuery.makeArray(setDifference);
-        return categoriesToHighlight.map(Scanner._createCategoryRegexp);
+        const categories = Scanner._findAllMatches(text, this._categoryRegexp)
+            .map(match => match[1]);
+        const attributes = Scanner._findAllMatches(text, this._attributeRegexp)
+            .map(match => match[1]);
+        const categoriesToHighlight = jQuery.makeArray(jQuery(categories).not(this._ontologies.classes));
+        const attributesToHighlight = attributes.filter(attr => this._isNotValidAttribute(attr));
+        return categoriesToHighlight.map(Scanner._createCategoryRegexp).concat(
+            attributesToHighlight.map(Scanner._createAttributeRegexp)
+        )
+    }
+
+    _isNotValidAttribute(attribute) {
+        return !this._ontologies.isRelation(attribute) &&
+            this._ontologies.searchProperties(this.categories, attribute).length === 0;
     }
 
     static _createCategoryRegexp(category) {
         return new RegExp(`\\[\\[category:(${category})]]`, 'g');
     }
+
+    static _createAttributeRegexp(attribute) {
+        return new RegExp(`\\[\\[(${attribute}):=[^\\]]+]]`, 'g');
+    }
+
 };
 module.exports.symbols = {
     ID : '[\\w\\-.]*|[\\w\\-.]*:[\\w\\-.]*'
